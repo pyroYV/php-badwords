d5281535bafa65d380156e8cbd0e96ea20ab886d
pyroYV <simonerizzo.pvt@gmail.com>
2022-08-22 12:59:48 +0200

    Milestone

--- index.php
+++ index.php
@@ -9,11 +9,20 @@
 <body>
     <h1>
         <?php
-        $paragraph = 'ciao sono proprio un bravo programmatore che non dice mai 404';
-
-        echo $paragraph
-
+        $paragraph = 'ciao sono proprio un bravo programmatore che non dice mai 404 e poi 404 e ancora di più 404';
         
+        echo $paragraph;
+        
+        //?censura=404
+        $subs = $_GET['censura']; 
+        ?>
+    </h1>
+
+    <h1>
+        Censurato:
+        <?php
+        $paragraph2 = str_replace($subs,'***',$paragraph);
+        echo $paragraph2;
         ?>
     </h1>
 </body>
